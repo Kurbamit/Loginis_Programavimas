adc594ccdd4a08ac2152c0cc5db69e621229cd9f
Dominykas Černovas <cernovas50@gmail.com>
2024-09-10 18:40:20 +0300

    1.2 užduotis

--- 2_Lab/2LP.pl
+++ 2_Lab/2LP.pl
@@ -1,10 +1,34 @@
 /* Dominykas Černovas, 3 kursas, 3 grupė */
 /* Užduoties variantai: 1.2, 4.1 */
 
+/* DUOMENYS */
+kelias(palanga, klaipeda, 60).
+kelias(palanga, taurage, 30).
+kelias(palanga, kaunas, 200).
+kelias(palanga, telsiai, 20).
+kelias(telsiai, siauliai, 70).
+kelias(siauliai, panevezys, 80).
+kelias(panevezys, vilnius, 140).
+kelias(klaipeda, vilnius, 310).
+kelias(taurage, kaunas, 130).
+kelias(kaunas, vilnius, 100).
+
 /* 
 1.2 Apibrėžkite predikatą „galima nuvažiuoti iš miesto X į miestą Y“: nuvažiavus lygiai L kilometrų.
+Testai:
+kelione(palanga, klaipeda, 60) - true
+kelione(palanga, vilnius, 500) - false
+kelione(palanga, vilnius, 370) - true
+kelione(palanga, vilnius, 260) - true
+kelione(palanga, vilnius, Atstumas) - Atstumas = 370 ; Atstumas = 260 ; Atstumas = 300 ; Atstumas = 310;
 */
-
+kelione(Miestas1, Miestas2, Atstumas) :- 
+    kelias(Miestas1, Miestas2, Atstumas),
+    !.
+kelione(Miestas1, Miestas2, Atstumas) :-
+    kelias(Miestas1, MiestasTemp, Atstumas1),
+    kelione(MiestasTemp, Miestas2, Atstumas2),
+    Atstumas is Atstumas1 + Atstumas2.
 
 /* 
 4.1 Duotas natūrinis skaičius N. Naudodami rekursyvųjį algoritmą, raskite N-ąjį Fibonačio skaičių.
